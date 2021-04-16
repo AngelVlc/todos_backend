bf0f350455679b2dcf7ab35a9d484d9e0826acfd
AngelVlc <angel.ballesteros@domestika.org>
2021-04-16 22:50:31 +0200

    Fixes e2e tests

--- src/cmd/api/main_e2e_test.go
+++ src/cmd/api/main_e2e_test.go
@@ -20,54 +20,56 @@ import (
 )
 
 func TestEndtoEnd(t *testing.T) {
-	// baseURL := os.Getenv("BASE_URL")
-	// require.NotNil(t, baseURL)
+	baseURL := os.Getenv("BASE_URL")
+	require.NotNil(t, baseURL)
 
-	// adminPass := os.Getenv("ADMIN_PASSWORD")
-	// require.NotNil(t, adminPass)
+	adminPass := os.Getenv("ADMIN_PASSWORD")
+	require.NotNil(t, adminPass)
 
-	// client := &http.Client{}
+	client := &http.Client{}
 
-	// loginBody := "{\"username\": \"admin\",\"password\": \"admin\"}"
-	// req := createRequest(t, "POST", baseURL+"/auth/login", strings.NewReader(loginBody))
-	// req.Header.Set("Content-type", "application/json")
-	// res, err := client.Do(req)
-	// require.Nil(t, err)
-	// require.Equal(t, 200, res.StatusCode)
-	// tokenRes := authDomain.TokenResponse{}
-	// err = objFromRes(res.Body, &tokenRes)
-	// require.Nil(t, err)
+	loginBody := fmt.Sprintf("{\"username\": \"admin\",\"password\": \"%v\"}", adminPass)
+	req := createRequest(t, "POST", baseURL+"/auth/login", strings.NewReader(loginBody), nil)
+	req.Header.Set("Content-type", "application/json")
+	res, err := client.Do(req)
+	require.Nil(t, err)
+	require.Equal(t, 200, res.StatusCode)
+	loginRes := authDomain.LoginResponse{}
+	err = objFromRes(res.Body, &loginRes)
+	require.Nil(t, err)
 
-	// authHeaderContent := fmt.Sprintf("Bearer %v", tokenRes.Token)
+	loginResCookies := res.Cookies()
 
-	// req = createRequest(t, "GET", baseURL+"/users/1", nil)
-	// req.Header.Set("Authorization", authHeaderContent)
-	// res, err = client.Do(req)
-	// require.Nil(t, err)
-	// require.Equal(t, 200, res.StatusCode)
-	// userRes := authInfra.UserResponse{}
-	// err = objFromRes(res.Body, &userRes)
-	// require.Nil(t, err)
-	// require.True(t, userRes.IsAdmin)
+	req = createRequest(t, "GET", baseURL+"/users/1", nil, loginResCookies)
+	res, err = client.Do(req)
+	require.Nil(t, err)
+	require.Equal(t, 200, res.StatusCode)
+	userRes := authInfra.UserResponse{}
+	err = objFromRes(res.Body, &userRes)
+	require.Nil(t, err)
+	require.True(t, userRes.IsAdmin)
 
-	// listName := fmt.Sprintf("test %v", time.Now().Format("2006-01-02T15:04:05-0700"))
-	// listBody := fmt.Sprintf("{\"name\": \"%v\"}", listName)
-	// req = createRequest(t, "POST", baseURL+"/lists", strings.NewReader(listBody))
-	// req.Header.Set("Authorization", authHeaderContent)
-	// req.Header.Set("Content-type", "application/json")
-	// res, err = client.Do(req)
-	// require.Nil(t, err)
-	// require.Equal(t, 200, res.StatusCode)
-	// createdRes := listsDomain.List{}
-	// err = objFromRes(res.Body, &createdRes)
-	// require.Nil(t, err)
-	// listID := fmt.Sprint(createdRes.ID)
+	listName := fmt.Sprintf("test %v", time.Now().Format("2006-01-02T15:04:05-0700"))
+	listBody := fmt.Sprintf("{\"name\": \"%v\"}", listName)
+	req = createRequest(t, "POST", baseURL+"/lists", strings.NewReader(listBody), loginResCookies)
+	res, err = client.Do(req)
+	require.Nil(t, err)
+	require.Equal(t, 200, res.StatusCode)
+	createdRes := listsDomain.List{}
+	err = objFromRes(res.Body, &createdRes)
+	require.Nil(t, err)
+	listID := fmt.Sprint(createdRes.ID)
 
-	// req = createRequest(t, "DELETE", baseURL+"/lists/"+string(listID), nil)
-	// req.Header.Set("Authorization", authHeaderContent)
-	// res, err = client.Do(req)
-	// require.Nil(t, err)
-	// require.Equal(t, 204, res.StatusCode)
+	req = createRequest(t, "DELETE", baseURL+"/lists/"+string(listID), nil, loginResCookies)
+	res, err = client.Do(req)
+	require.Nil(t, err)
+	require.Equal(t, 204, res.StatusCode)
+
+	req = createRequest(t, "POST", baseURL+"/auth/refreshtoken", nil, loginResCookies)
+	res, err = client.Do(req)
+	require.Nil(t, err)
+	require.Equal(t, 200, res.StatusCode)
+	require.Equal(t, 1, len(res.Cookies()))
 }
 
 func bufferFromBody(body interface{}) (*bytes.Buffer, error) {
@@ -85,9 +87,15 @@ func objFromRes(resBody io.Reader, obj interface{}) error {
 	return err
 }
 
-func createRequest(t *testing.T, method, url string, body io.Reader) *http.Request {
+func createRequest(t *testing.T, method, url string, body io.Reader, cookies []*http.Cookie) *http.Request {
 	req, err := http.NewRequest(method, url, body)
 	require.Nil(t, err)
+
+	if cookies != nil {
+		for _, v := range cookies {
+			req.AddCookie(v)
+		}
+	}
 
 	return req
 }

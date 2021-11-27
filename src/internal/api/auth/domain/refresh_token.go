a21035cf87b001b47200c97fa0f960d26a00a514
AngelVlc <angel.ballesteros@domestika.org>
2021-11-27 10:14:57 +0100

    Fixes gorm index in refresh token model

--- src/internal/api/auth/domain/refresh_token.go
+++ src/internal/api/auth/domain/refresh_token.go
@@ -5,7 +5,7 @@ import "time"
 type RefreshToken struct {
 	ID             int32     `gorm:"type:int(32);primary_key"`
 	UserID         int32     `gorm:"column:userId;type:int(32)"`
-	RefreshToken   string    `gorm:"column:refreshToken;type:varchar(250);index:idx_refresh_token"`
+	RefreshToken   string    `gorm:"column:refreshToken;type:varchar(250);index:idx_refresh_token,unique"`
 	ExpirationDate time.Time `gorm:"column:expirationDate;type:timestamp"`
 }
 

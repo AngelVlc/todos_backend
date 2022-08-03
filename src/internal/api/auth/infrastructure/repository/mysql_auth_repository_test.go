a354456e5d29e94135d2ce6101199d912fac546c
AngelVlc <angel.ballesteros@domestika.org>
2022-08-03 17:06:19 +0200

    Refactors auth/infrastructure/repository tests

--- src/internal/api/auth/infrastructure/repository/mysql_auth_repository_test.go
+++ src/internal/api/auth/infrastructure/repository/mysql_auth_repository_test.go
@@ -23,7 +23,7 @@ var (
 	refreshTokenColumns = []string{"id", "userId", "refreshToken", "expirationDate"}
 )
 
-func TestMySqlAuthRepositoryFindRefreshTokenForUser(t *testing.T) {
+func TestMySqlAuthRepository_FindRefreshTokenForUser_Does_Not_Return_A_RefreshToken_If_Does_Not_Exist(t *testing.T) {
 	mock, db := helpers.GetMockedDb(t)
 	repo := NewMySqlAuthRepository(db)
 
@@ -35,43 +35,63 @@ func TestMySqlAuthRepositoryFindRefreshTokenForUser(t *testing.T) {
 			WithArgs(userID, rt)
 	}
 
-	t.Run("should not return a refresh token if it does not exist", func(t *testing.T) {
-		expectedFindByIDQuery().WillReturnRows(sqlmock.NewRows(refreshTokenColumns))
+	expectedFindByIDQuery().WillReturnRows(sqlmock.NewRows(refreshTokenColumns))
 
-		res, err := repo.FindRefreshTokenForUser(context.Background(), rt, userID)
+	res, err := repo.FindRefreshTokenForUser(context.Background(), rt, userID)
 
-		assert.Nil(t, res)
-		assert.Nil(t, err)
+	assert.Nil(t, res)
+	assert.Nil(t, err)
 
-		helpers.CheckSqlMockExpectations(mock, t)
-	})
-
-	t.Run("should return an error if the query fails", func(t *testing.T) {
-		expectedFindByIDQuery().WillReturnError(fmt.Errorf("some error"))
-
-		res, err := repo.FindRefreshTokenForUser(context.Background(), rt, userID)
-
-		assert.Nil(t, res)
-		assert.EqualError(t, err, "some error")
-
-		helpers.CheckSqlMockExpectations(mock, t)
-	})
-
-	t.Run("should return the refresh token if it exists", func(t *testing.T) {
-		expectedFindByIDQuery().WillReturnRows(sqlmock.NewRows(refreshTokenColumns).AddRow(int32(111), userID, rt, time.Now()))
-
-		res, err := repo.FindRefreshTokenForUser(context.Background(), rt, userID)
-
-		require.NotNil(t, res)
-		assert.Equal(t, userID, res.UserID)
-		assert.Equal(t, int32(111), res.ID)
-		assert.Nil(t, err)
-
-		helpers.CheckSqlMockExpectations(mock, t)
-	})
+	helpers.CheckSqlMockExpectations(mock, t)
 }
 
-func TestMySqlAuthRepositoryCreateRefreshTokenIfNotExist(t *testing.T) {
+func TestMySqlAuthRepository_FindRefreshTokenForUser_Returns_An_Error_If_The_Query_Fails(t *testing.T) {
+	mock, db := helpers.GetMockedDb(t)
+	repo := NewMySqlAuthRepository(db)
+
+	rt := "rt"
+	userID := int32(1)
+
+	expectedFindByIDQuery := func() *sqlmock.ExpectedQuery {
+		return mock.ExpectQuery(regexp.QuoteMeta("SELECT * FROM `refresh_tokens` WHERE `refresh_tokens`.`userId` = ? AND `refresh_tokens`.`refreshToken` = ? LIMIT 1")).
+			WithArgs(userID, rt)
+	}
+
+	expectedFindByIDQuery().WillReturnError(fmt.Errorf("some error"))
+
+	res, err := repo.FindRefreshTokenForUser(context.Background(), rt, userID)
+
+	assert.Nil(t, res)
+	assert.EqualError(t, err, "some error")
+
+	helpers.CheckSqlMockExpectations(mock, t)
+}
+
+func TestMySqlAuthRepository_FindRefreshTokenForUser_Returns_A_RefreshToken_If_Exists(t *testing.T) {
+	mock, db := helpers.GetMockedDb(t)
+	repo := NewMySqlAuthRepository(db)
+
+	rt := "rt"
+	userID := int32(1)
+
+	expectedFindByIDQuery := func() *sqlmock.ExpectedQuery {
+		return mock.ExpectQuery(regexp.QuoteMeta("SELECT * FROM `refresh_tokens` WHERE `refresh_tokens`.`userId` = ? AND `refresh_tokens`.`refreshToken` = ? LIMIT 1")).
+			WithArgs(userID, rt)
+	}
+
+	expectedFindByIDQuery().WillReturnRows(sqlmock.NewRows(refreshTokenColumns).AddRow(int32(111), userID, rt, time.Now()))
+
+	res, err := repo.FindRefreshTokenForUser(context.Background(), rt, userID)
+
+	require.NotNil(t, res)
+	assert.Equal(t, userID, res.UserID)
+	assert.Equal(t, int32(111), res.ID)
+	assert.Nil(t, err)
+
+	helpers.CheckSqlMockExpectations(mock, t)
+}
+
+func TestMySqlAuthRepository_CreateRefreshTokenIfNotExist_Returns_An_Error_If_Creating_The_New_RefreshToken_Fails(t *testing.T) {
 	mock, db := helpers.GetMockedDb(t)
 	repo := NewMySqlAuthRepository(db)
 
@@ -83,34 +103,43 @@ func TestMySqlAuthRepositoryCreateRefreshTokenIfNotExist(t *testing.T) {
 			WithArgs(rt.UserID, rt.RefreshToken, rt.ExpirationDate)
 	}
 
-	t.Run("should return an error if creating the new refresh token fails", func(t *testing.T) {
-		mock.ExpectBegin()
-		expectedInsertExec().WillReturnError(fmt.Errorf("some error"))
-		mock.ExpectRollback()
+	mock.ExpectBegin()
+	expectedInsertExec().WillReturnError(fmt.Errorf("some error"))
+	mock.ExpectRollback()
 
-		err := repo.CreateRefreshTokenIfNotExist(context.Background(), &rt)
+	err := repo.CreateRefreshTokenIfNotExist(context.Background(), &rt)
 
-		assert.EqualError(t, err, "some error")
+	assert.EqualError(t, err, "some error")
 
-		helpers.CheckSqlMockExpectations(mock, t)
-	})
-
-	t.Run("should create the new refresh token", func(t *testing.T) {
-		result := sqlmock.NewResult(12, 1)
-
-		mock.ExpectBegin()
-		expectedInsertExec().WillReturnResult(result)
-		mock.ExpectCommit()
-
-		err := repo.CreateRefreshTokenIfNotExist(context.Background(), &rt)
-
-		assert.Nil(t, err)
-
-		helpers.CheckSqlMockExpectations(mock, t)
-	})
+	helpers.CheckSqlMockExpectations(mock, t)
 }
 
-func TestMySqlAuthDeleteExpiredRefreshTokens(t *testing.T) {
+func TestMySqlAuthRepository_CreateRefreshTokenIfNotExist_Creates_A_New_RefreshToken(t *testing.T) {
+	mock, db := helpers.GetMockedDb(t)
+	repo := NewMySqlAuthRepository(db)
+
+	expDate, _ := time.Parse("2021-Jan-01", "2014-Feb-04")
+	rt := domain.RefreshToken{UserID: 1, RefreshToken: "rt", ExpirationDate: expDate}
+
+	expectedInsertExec := func() *sqlmock.ExpectedExec {
+		return mock.ExpectExec(regexp.QuoteMeta("INSERT INTO `refresh_tokens` (`userId`,`refreshToken`,`expirationDate`) VALUES (?,?,?) ON DUPLICATE KEY UPDATE `id`=`id`")).
+			WithArgs(rt.UserID, rt.RefreshToken, rt.ExpirationDate)
+	}
+
+	result := sqlmock.NewResult(12, 1)
+
+	mock.ExpectBegin()
+	expectedInsertExec().WillReturnResult(result)
+	mock.ExpectCommit()
+
+	err := repo.CreateRefreshTokenIfNotExist(context.Background(), &rt)
+
+	assert.Nil(t, err)
+
+	helpers.CheckSqlMockExpectations(mock, t)
+}
+
+func TestMySqlAuth_DeleteExpiredRefreshTokens_Returns_An_Error_If_The_Delete_Fails(t *testing.T) {
 	mock, db := helpers.GetMockedDb(t)
 	repo := NewMySqlAuthRepository(db)
 
@@ -119,35 +148,41 @@ func TestMySqlAuthDeleteExpiredRefreshTokens(t *testing.T) {
 			WithArgs(expTime)
 	}
 
-	t.Run("should return an error if creating the new refresh token fails", func(t *testing.T) {
-		now := time.Now()
-		mock.ExpectBegin()
-		expectedDelete(now).WillReturnError(fmt.Errorf("some error"))
-		mock.ExpectRollback()
+	now := time.Now()
+	mock.ExpectBegin()
+	expectedDelete(now).WillReturnError(fmt.Errorf("some error"))
+	mock.ExpectRollback()
 
-		err := repo.DeleteExpiredRefreshTokens(context.Background(), now)
+	err := repo.DeleteExpiredRefreshTokens(context.Background(), now)
 
-		assert.EqualError(t, err, "some error")
+	assert.EqualError(t, err, "some error")
 
-		helpers.CheckSqlMockExpectations(mock, t)
-	})
-
-	t.Run("should create the new refresh token", func(t *testing.T) {
-		now := time.Now()
-
-		mock.ExpectBegin()
-		expectedDelete(now).WillReturnResult(sqlmock.NewResult(0, 0))
-		mock.ExpectCommit()
-
-		err := repo.DeleteExpiredRefreshTokens(context.Background(), now)
-
-		assert.Nil(t, err)
-
-		helpers.CheckSqlMockExpectations(mock, t)
-	})
+	helpers.CheckSqlMockExpectations(mock, t)
 }
 
-func TestMySqlAuthRepositoryGetAllRefreshTokens(t *testing.T) {
+func TestMySqlAuth_DeleteExpiredRefreshTokens_Deletes_The_Expired_RefreshTokens(t *testing.T) {
+	mock, db := helpers.GetMockedDb(t)
+	repo := NewMySqlAuthRepository(db)
+
+	expectedDelete := func(expTime time.Time) *sqlmock.ExpectedExec {
+		return mock.ExpectExec(regexp.QuoteMeta("DELETE FROM `refresh_tokens` WHERE expirationDate <= ?")).
+			WithArgs(expTime)
+	}
+
+	now := time.Now()
+
+	mock.ExpectBegin()
+	expectedDelete(now).WillReturnResult(sqlmock.NewResult(0, 0))
+	mock.ExpectCommit()
+
+	err := repo.DeleteExpiredRefreshTokens(context.Background(), now)
+
+	assert.Nil(t, err)
+
+	helpers.CheckSqlMockExpectations(mock, t)
+}
+
+func TestMySqlAuthRepository_GetAllRefreshTokens_Returns_An_Error_If_The_Query_Fails(t *testing.T) {
 	mock, db := helpers.GetMockedDb(t)
 	repo := NewMySqlAuthRepository(db)
 
@@ -157,35 +192,42 @@ func TestMySqlAuthRepositoryGetAllRefreshTokens(t *testing.T) {
 		return mock.ExpectQuery("SELECT id,userId,expirationDate FROM `refresh_tokens` ORDER BY expirationDate asc LIMIT 10 OFFSET 10")
 	}
 
-	t.Run("should return an error if the query fails", func(t *testing.T) {
-		expectedGetQuery().WillReturnError(fmt.Errorf("some error"))
+	expectedGetQuery().WillReturnError(fmt.Errorf("some error"))
 
-		res, err := repo.GetAllRefreshTokens(context.Background(), paginationInfo)
+	res, err := repo.GetAllRefreshTokens(context.Background(), paginationInfo)
 
-		assert.Nil(t, res)
-		assert.EqualError(t, err, "some error")
+	assert.Nil(t, res)
+	assert.EqualError(t, err, "some error")
 
-		helpers.CheckSqlMockExpectations(mock, t)
-	})
-
-	t.Run("should return the users", func(t *testing.T) {
-		columns := []string{"id", "userId", "expirationDate"}
-		now := time.Now()
-		expectedGetQuery().WillReturnRows(sqlmock.NewRows(columns).AddRow(11, 1, now))
-
-		res, err := repo.GetAllRefreshTokens(context.Background(), paginationInfo)
-
-		assert.Nil(t, err)
-		require.Equal(t, 1, len(res))
-		assert.Equal(t, int32(11), res[0].ID)
-		assert.Equal(t, int32(1), res[0].UserID)
-		assert.Equal(t, now, res[0].ExpirationDate)
-
-		helpers.CheckSqlMockExpectations(mock, t)
-	})
+	helpers.CheckSqlMockExpectations(mock, t)
 }
 
-func TestMySqlAuthRepositoryDeleteRefreshTokensByID(t *testing.T) {
+func TestMySqlAuthRepository_GetAllRefreshTokens_Returns_The_RefreshTokens(t *testing.T) {
+	mock, db := helpers.GetMockedDb(t)
+	repo := NewMySqlAuthRepository(db)
+
+	paginationInfo := sharedDomain.NewPaginationInfo(10, 10, "expirationDate", sharedDomain.OrderAsc)
+
+	expectedGetQuery := func() *sqlmock.ExpectedQuery {
+		return mock.ExpectQuery("SELECT id,userId,expirationDate FROM `refresh_tokens` ORDER BY expirationDate asc LIMIT 10 OFFSET 10")
+	}
+
+	columns := []string{"id", "userId", "expirationDate"}
+	now := time.Now()
+	expectedGetQuery().WillReturnRows(sqlmock.NewRows(columns).AddRow(11, 1, now))
+
+	res, err := repo.GetAllRefreshTokens(context.Background(), paginationInfo)
+
+	assert.Nil(t, err)
+	require.Equal(t, 1, len(res))
+	assert.Equal(t, int32(11), res[0].ID)
+	assert.Equal(t, int32(1), res[0].UserID)
+	assert.Equal(t, now, res[0].ExpirationDate)
+
+	helpers.CheckSqlMockExpectations(mock, t)
+}
+
+func TestMySqlAuthRepository_DeleteRefreshTokensByID_Returns_An_Error_If_The_Delete_Fails(t *testing.T) {
 	mock, db := helpers.GetMockedDb(t)
 	repo := NewMySqlAuthRepository(db)
 
@@ -196,25 +238,33 @@ func TestMySqlAuthRepositoryDeleteRefreshTokensByID(t *testing.T) {
 
 	ids := []int32{int32(1), int32(2), int32(3)}
 
-	t.Run("should return an error if delete fails", func(t *testing.T) {
-		mock.ExpectBegin()
-		expectedDeleteExec().WillReturnError(fmt.Errorf("some error"))
-		mock.ExpectRollback()
+	mock.ExpectBegin()
+	expectedDeleteExec().WillReturnError(fmt.Errorf("some error"))
+	mock.ExpectRollback()
 
-		err := repo.DeleteRefreshTokensByID(context.Background(), ids)
+	err := repo.DeleteRefreshTokensByID(context.Background(), ids)
 
-		assert.EqualError(t, err, "some error")
-		helpers.CheckSqlMockExpectations(mock, t)
-	})
+	assert.EqualError(t, err, "some error")
+	helpers.CheckSqlMockExpectations(mock, t)
+}
 
-	t.Run("should delete the refresh tokens", func(t *testing.T) {
-		mock.ExpectBegin()
-		expectedDeleteExec().WillReturnResult(sqlmock.NewResult(0, 0))
-		mock.ExpectCommit()
+func TestMySqlAuthRepository_DeleteRefreshTokensByID_Deletes_The_RefreshTokens(t *testing.T) {
+	mock, db := helpers.GetMockedDb(t)
+	repo := NewMySqlAuthRepository(db)
 
-		err := repo.DeleteRefreshTokensByID(context.Background(), ids)
+	expectedDeleteExec := func() *sqlmock.ExpectedExec {
+		return mock.ExpectExec(regexp.QuoteMeta("DELETE FROM `refresh_tokens` WHERE `refresh_tokens`.`id` IN (?,?,?)")).
+			WithArgs(int32(1), int32(2), int32(3))
+	}
 
-		assert.Nil(t, err)
-		helpers.CheckSqlMockExpectations(mock, t)
-	})
+	ids := []int32{int32(1), int32(2), int32(3)}
+
+	mock.ExpectBegin()
+	expectedDeleteExec().WillReturnResult(sqlmock.NewResult(0, 0))
+	mock.ExpectCommit()
+
+	err := repo.DeleteRefreshTokensByID(context.Background(), ids)
+
+	assert.Nil(t, err)
+	helpers.CheckSqlMockExpectations(mock, t)
 }

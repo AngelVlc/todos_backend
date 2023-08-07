31fc0877fe142b6a2fd1b20a5dbcfefbf75c887a
Angel Ballesteros <angelb@237global.com>
2023-08-07 10:37:15 +0200

    Move item working

---
+++ src/internal/api/lists/application/move_list_item_service.go
@@ -0,0 +1,63 @@
+package application
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/AngelVlc/todos_backend/src/internal/api/lists/domain"
+	appErrors "github.com/AngelVlc/todos_backend/src/internal/api/shared/domain/errors"
+	"github.com/AngelVlc/todos_backend/src/internal/api/shared/domain/events"
+)
+
+type MoveListItemService struct {
+	repo     domain.ListsRepository
+	eventBus events.EventBus
+}
+
+func NewMoveListItemService(repo domain.ListsRepository, eventBus events.EventBus) *MoveListItemService {
+	return &MoveListItemService{repo, eventBus}
+}
+
+func (s *MoveListItemService) MoveListItem(ctx context.Context, originListID int32, originListItemID int32, destinationListID int32, userID int32) error {
+	foundOriginList, err := s.repo.FindList(ctx, domain.ListEntity{ID: originListID, UserID: userID})
+	if err != nil {
+		return err
+	}
+
+	foundDestinationList, err := s.repo.FindList(ctx, domain.ListEntity{ID: destinationListID, UserID: userID})
+	if err != nil {
+		return &appErrors.BadRequestError{Msg: "The destination list does not exist"}
+	}
+
+	indexToRemove := -1
+
+	for i, item := range foundOriginList.Items {
+		if item.ID == originListItemID {
+			indexToRemove = i
+			item.ListID = destinationListID
+			item.Position = foundDestinationList.GetMaxItemPosition() + 1
+			foundDestinationList.Items = append(foundDestinationList.Items, item)
+
+			break
+		}
+	}
+
+	if indexToRemove < 0 {
+		return &appErrors.BadRequestError{Msg: fmt.Sprintf("An item with id %v doesn't exist in the original list", originListItemID)}
+	}
+
+	foundOriginList.Items = append(foundOriginList.Items[:indexToRemove], foundOriginList.Items[indexToRemove+1:]...)
+
+	if _, err = s.repo.UpdateList(ctx, foundOriginList); err != nil {
+		return &appErrors.UnexpectedError{Msg: "Error updating the original list", InternalError: err}
+	}
+
+	if _, err = s.repo.UpdateList(ctx, foundDestinationList); err != nil {
+		return &appErrors.UnexpectedError{Msg: "Error updating the destination list", InternalError: err}
+	}
+
+	go s.eventBus.Publish("listCreatedOrUpdated", foundOriginList.ID)
+	go s.eventBus.Publish("listCreatedOrUpdated", foundDestinationList.ID)
+
+	return nil
+}
